5e469035b926e047d6f0348caf4a52ae922de3c3
niu-grandpa <2864103063@qq.com>
2022-10-29 13:22:29 +0800

    增加useDidMount

--- packages/core/hooks/useDidMount.ts
+++ packages/core/hooks/useDidMount.ts
@@ -1,8 +1,15 @@
 import { useEffect } from "react";
+import { CallbackWithNoArguments } from "../types/types";
 
-export function useDidMount(fn: () => any, deps?: any[]) {
+/**
+ * useDidMount
+ * @description 该函数会在组件挂载时触发。
+ *
+ * @param {Function} effect 执行的副作用函数
+ */
+export function useDidMount(effect: CallbackWithNoArguments): void {
   useEffect(() => {
-    const cleanup = fn();
+    const cleanup = effect();
     return cleanup;
-  }, deps);
+  }, []);
 }
