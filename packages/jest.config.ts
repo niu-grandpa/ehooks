a91c169703cf876e79d94d619a12c3930f57d6ec
niu-grandpa <2864103063@qq.com>
2022-10-29 13:40:01 +0800

    修改配置文件

--- packages/jest.config.ts
+++ packages/jest.config.ts
@@ -1,7 +1,5 @@
 // jest.config.ts
 import type { Config } from "@jest/types";
-import { pathsToModuleNameMapper } from "ts-jest";
-const { compilerOptions } = require("./tsconfig");
 // Or async function
 
 export default (): Config.InitialOptions => {
@@ -9,11 +7,17 @@ export default (): Config.InitialOptions => {
     preset: "ts-jest",
     testEnvironment: "jsdom",
     testMatch: ["<rootDir>/__tests__/**/*.(spec|test).(ts|tsx)"],
+    testPathIgnorePatterns: ["node_modules/"],
     verbose: true,
-    // transform: {
-    //   "^.+\\.[jt]sx?$": "esbuild-jest",
-    // },
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+    transform: {
+      "^.+.tsx?$": "ts-jest",
+    },
+    moduleNameMapper: {
+      // Mocks out all these file formats when tests are run
+      ".(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "identity-obj-proxy",
+      ".(css|less|scss|sass)$": "identity-obj-proxy",
+    },
     rootDir: "./",
     coverageProvider: "v8",
     modulePaths: ["<rootDir>"],
--- packages/typeing.d.ts
+++ packages/typeing.d.ts
@@ -1,4 +1,3 @@
 declare module "rollup-plugin-babel";
 declare module "rollup-plugin-clear";
 declare module "rollup-plugin-eslint";
-declare module "@testing-library/jest-dom/extend-expect";
