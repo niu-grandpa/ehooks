9919e1c6521978758b6fd71c9c80a7b946d31a7d
niu-grandpa <2864103063@qq.com>
2022-11-09 23:03:56 +0800

    useThrottle (1/2)

--- packages/core/hooks/useThrottle.ts
+++ packages/core/hooks/useThrottle.ts
@@ -1,8 +1,44 @@
+import { useCallback, useRef } from "react";
+import { CallbackWithNoArguments } from "../types/types";
+import { useDidMount } from "./useDidMount";
+import { useWillUnmount } from "./useWillUnmount";
+
 /**
  * useThrottle
  *
- * @description 在n秒内只会触发高频事件一次，稀释函数的执行频率。
+ * @description 在n秒后执行事件函数，稀释函数执行频率。
  */
-export function useThrottle() {
-  // TODO
+export function useThrottle(callback: CallbackWithNoArguments, wait?: number) {
+  const timerId = useRef<NodeJS.Timeout | null>(null);
+  const fn = useRef<CallbackWithNoArguments | null>(callback);
+
+  if (typeof callback !== "function") {
+    throw TypeError("expected is a function!");
+  }
+
+  // TODO arguments和调用this指向
+  const throttleFn = useCallback(() => {
+    if (timerId.current) return;
+    timerId.current = setTimeout(() => {
+      clearTimer(timerId.current);
+      callback();
+    }, wait || 200);
+  }, [timerId.current]);
+
+  useDidMount(() => {
+    fn.current = throttleFn;
+  });
+
+  useWillUnmount(() => {
+    clearTimer(timerId.current);
+    fn.current = null;
+  });
+
+  return fn.current;
 }
+
+export const clearTimer = (timerId: NodeJS.Timeout | null) => {
+  if (!timerId) return;
+  clearTimeout(timerId);
+  timerId = null;
+};
