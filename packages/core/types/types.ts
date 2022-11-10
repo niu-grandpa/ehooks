19a92805504b5855b0fb74ea6deacab2b2688c86
niu-grandpa <2864103063@qq.com>
2022-11-10 23:08:27 +0800

    update

--- packages/core/types/types.ts
+++ packages/core/types/types.ts
@@ -2,3 +2,4 @@ export type CallbackOfAnyType = () => any;
 export type CallbackWithNoArguments = () => void;
 export type CallbackWithArguments<T = unknown> = (...args: T[]) => void;
 export type Key = string | number | symbol;
+export type UnknownFunction = (...args: unknown[]) => unknown;
