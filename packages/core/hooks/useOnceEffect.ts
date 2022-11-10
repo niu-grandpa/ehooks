e338322c4babc3c5482c68ea4dbb7715564757fa
niu-grandpa <2864103063@qq.com>
2022-11-10 23:08:44 +0800

    update useOnceEffect

--- packages/core/hooks/useOnceEffect.ts
+++ packages/core/hooks/useOnceEffect.ts
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { CallbackWithNoArguments } from "../types/types";
+import { useWillUnmount } from "./useWillUnmount";
 
 /**
  * useOnceEffect
  * @description 函数仅会触发一次，即使依赖发生变化。
  *
- * @param {Function} effect 回调
+ * @param {Function} effect 执行的副作用函数
  * @param {React.DependencyList} deps 函数依赖项
  * @return {Boolean} 返回一个布尔值，表示是否已经调用。
  */
@@ -13,14 +14,20 @@ export function useOnceEffect(
   effect: CallbackWithNoArguments,
   deps?: React.DependencyList
 ): boolean {
-  let isCall = false;
+  const isCall = useRef<boolean>(false);
+  const cleanup = useRef<CallbackWithNoArguments | void>();
 
   useEffect(() => {
-    if (!isCall) {
-      isCall = true;
-      return effect && effect();
+    if (!isCall.current) {
+      isCall.current = true;
+      cleanup.current = effect && effect();
     }
   }, deps || []);
 
-  return isCall;
+  useWillUnmount(() => {
+    isCall.current = false;
+    typeof cleanup.current === "function" && cleanup.current();
+  });
+
+  return isCall.current;
 }
