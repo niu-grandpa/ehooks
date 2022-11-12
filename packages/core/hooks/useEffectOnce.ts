b158a48ef214abcc5a2887d1f12ac9f6b14d249f
niu-grandpa <2864103063@qq.com>
2022-11-12 15:00:53 +0800

    useEffectOnce

--- packages/core/hooks/useEffectOnce.ts
+++ packages/core/hooks/useEffectOnce.ts
@@ -1,32 +1,39 @@
 import { useEffect, useRef } from "react";
 import { CallbackWithNoArguments } from "../types/types";
+import { useDidMount } from "./useDidMount";
 import { useWillUnmount } from "./useWillUnmount";
 
 /**
- * useOnceEffect
+ * useEffectOnce
  * @description 函数仅会触发一次，即使依赖发生变化。
  *
  * @param {Function} effect 执行的副作用函数
  * @param {React.DependencyList} deps 函数依赖项
  * @return {Boolean} 返回一个布尔值，表示是否已经调用。
  */
-export function useOnceEffect(
-  effect: CallbackWithNoArguments,
+export function useOnceEffect<T extends CallbackWithNoArguments>(
+  effect: T,
   deps?: React.DependencyList
 ): boolean {
   const isCall = useRef<boolean>(false);
-  const cleanup = useRef<CallbackWithNoArguments | void>();
+  const effectFn = useRef<T | null>(effect);
+  const cleanup = useRef<T | void>();
+
+  useDidMount(() => {
+    effectFn.current = effect;
+  });
 
   useEffect(() => {
     if (!isCall.current) {
       isCall.current = true;
-      cleanup.current = effect && effect();
+      cleanup.current = effectFn.current?.();
     }
   }, deps || []);
 
   useWillUnmount(() => {
     isCall.current = false;
-    typeof cleanup.current === "function" && cleanup.current();
+    cleanup.current?.();
+    effectFn.current = null;
   });
 
   return isCall.current;
