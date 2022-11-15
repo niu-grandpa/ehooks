b91cbe637856621604ff0b4b989c8c114fedcbef
niu-grandpa <2864103063@qq.com>
2022-11-15 22:06:14 +0800

    修改

--- packages/core/hooks/useGetIsMounted.ts
+++ packages/core/hooks/useGetIsMounted.ts
@@ -2,15 +2,12 @@ import { useCallback, useRef } from "react";
 import { useDidMount } from "./useDidMount";
 import { useWillUnmount } from "./useWillUnmount";
 
-type UseGetIsMounted = () => boolean;
-
 /**
  * useGetIsMounted
  *
  * @description 返回一个函数并调用它，用于判断当前组件是否已经挂载完毕。
- * @returns {UseGetIsMounted}
  */
-export function useGetIsMounted(): UseGetIsMounted {
+export function useGetIsMounted(): () => boolean {
   const isMounted = useRef<boolean>(false);
 
   useDidMount(() => {
