290f8de40839898f7a96ea5ec39c09c68e78a4fe
niu-grandpa <2864103063@qq.com>
2022-11-16 13:30:03 +0800

    增加useAsyncEffect

--- packages/core/hooks/useAsyncEffect.ts
+++ packages/core/hooks/useAsyncEffect.ts
@@ -8,11 +8,27 @@ type CleanupFn<T> = (result: T | void) => void;
 
 /**
  * useAsyncEffect
- * @description
+ * @description 在 useEffect 中能够使用异步函数
  *
- * @param {AsyncEffect<T>} effect
- * @param {React.DependencyList} deps
- * @param {CleanupFn} cleanup
+ * @param {AsyncEffect<T>} effect 异步回调
+ * @param {React.DependencyList} deps 依赖项，随着依赖变化自动更新副作用函数
+ * @param {CleanupFn} cleanup 组件卸载前的副作用清除函数，
+ * 通过其回调参数能够取到最新一次异步请求结果
+ * 
+ * @example
+ * ```ts
+  useAsyncEffect(async (isCallComplete) => {
+    const data = await requset('url');
+    if(isCallComplete()) {
+      const data2 = await requset('url');
+      // ...
+    }
+  }, 
+  [...deps], 
+  (result) => {
+     // do something
+  });
+ * ```
  */
 export function useAsyncEffect<T extends void>(
   effect: AsyncEffect<T>,
