67ad99a69bfb61df698ea8806fc6a602d6f317ab
niu-grandpa <2864103063@qq.com>
2022-11-24 13:25:10 +0800

    useFreshTick

---
+++ packages/core/hooks/useFreshTick.ts
@@ -0,0 +1,18 @@
+import { CallbackWithArguments } from "../types";
+import { useFreshRef } from "./useFreshRef";
+
+/**
+ * useFreshTick
+ * @description 将函数包装在ref里调用
+ *
+ * @param callback 组件挂载时调用
+ * @returns 返回一个新的回调函数
+ */
+export function useFreshTick<T>(callback: CallbackWithArguments<T>) {
+  const callbackRef = useFreshRef(callback);
+  const tick = (...args: T[]) => {
+    if (typeof callbackRef.current !== "function") return;
+    callbackRef.current(...args);
+  };
+  return tick;
+}
