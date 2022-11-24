faea37b57f8dce189f99ad873fabc9b26455c729
niu-grandpa <2864103063@qq.com>
2022-11-24 19:24:41 +0800

    update

--- packages/core/types/index.ts
+++ packages/core/types/index.ts
@@ -6,3 +6,12 @@ export type CallbackWithArguments<T = unknown> = (...args: T[]) => void;
 export type Key = string | number | symbol;
 export type UnknownFunction = (...args: unknown[]) => unknown;
 export type ReactElm = ReactElement | JSX.Element | ReactNode | string | number;
+
+export type ListenerOptions =
+  | boolean
+  | {
+      capture?: boolean;
+      once?: boolean;
+      passive?: boolean;
+      signal?: AbortSignal;
+    };
