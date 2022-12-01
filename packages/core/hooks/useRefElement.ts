19b73bbde8df52a0362c165ec6a76e7f004a8878
niu-grandpa <2864103063@qq.com>
2022-12-01 20:57:46 +0800

    useRefElement

---
+++ packages/core/hooks/useRefElement.ts
@@ -0,0 +1,20 @@
+import { useCallback, useMemo, useState } from "react";
+import { type RefElementOrNull } from "../types";
+
+/**
+ * useRefElement
+ * @description 通过回调的方式间接设置元素引用值
+ * @returns 返回一个变量引用元素和一个设置引用元素的回调
+ */
+export function useRefElement(): (
+  | ((element: RefElementOrNull<null>) => void)
+  | null
+)[] {
+  const [refElement, setRefElement] = useState<RefElementOrNull<null>>(null);
+
+  const ref = useCallback((element: RefElementOrNull<null>) => {
+    setRefElement(element);
+  }, []);
+
+  return useMemo(() => [refElement, ref], [refElement, ref]);
+}
--- packages/core/types/index.ts
+++ packages/core/types/index.ts
@@ -6,6 +6,7 @@ export type CallbackWithArguments<T = unknown> = (...args: T[]) => void;
 export type Key = string | number | symbol;
 export type UnknownFunction = (...args: unknown[]) => unknown;
 export type ReactElm = ReactElement | JSX.Element | ReactNode | string | number;
+export type RefElementOrNull<T> = T | null;
 
 export type ListenerOptions =
   | boolean
