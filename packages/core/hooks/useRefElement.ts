9744541bed0ea2eb2960deea0f99ed3b43eb5cf1
niu-grandpa <2864103063@qq.com>
2022-12-02 15:50:59 +0800

    update

--- packages/core/hooks/useRefElement.ts
+++ packages/core/hooks/useRefElement.ts
@@ -6,13 +6,13 @@ import { type RefElementOrNull } from "../types";
  * @description 通过回调的方式间接设置元素引用值
  * @returns 返回一个变量引用元素和一个设置引用元素的回调
  */
-export function useRefElement(): [
-  RefElementOrNull<null>,
-  (element: RefElementOrNull<null>) => void
+export function useRefElement<T>(): [
+  RefElementOrNull<T>,
+  (element: RefElementOrNull<T>) => void
 ] {
-  const [refElement, setRefElement] = useState<RefElementOrNull<null>>(null);
+  const [refElement, setRefElement] = useState<RefElementOrNull<T>>(null);
 
-  const ref = useCallback((element: RefElementOrNull<null>) => {
+  const ref = useCallback((element: RefElementOrNull<T>) => {
     setRefElement(element);
   }, []);
 
